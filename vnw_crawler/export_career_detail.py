8a1989eeb9dfdd36435f4594df14c02b3fd8b9c8
nerin321 <88995339+nerin321@users.noreply.github.com>
2023-06-14 17:00:51 +0700

    career detail

--- vnw_crawler/export_career_detail.py
+++ vnw_crawler/export_career_detail.py
@@ -10,6 +10,17 @@ query = {}
 
 query_area_result = exporter.find_area(query)
 query_career_result = exporter.find_career(query)
+query_job = exporter.find_job(query)
+
+skills = []
+for document in query_job:
+    if "detail" in document:
+        item = document["detail"]
+        for i in item:
+            skill = i['skills']
+        for i in skill:
+            if i not in skills:
+                skills.append(i) 
 
 export_area = []
 for area in query_area_result:
@@ -21,7 +32,7 @@ for career in query_career_result:
     career_name = career['name']
     export_career.append(career_name)
 
-def exported_career(career):
+def exported_follow_area(career):
     arr = []
     areas = []
     for area in export_area:
@@ -40,8 +51,7 @@ def exported_career(career):
         if count != 0:
             areas.append(area)
             arr.append(count)
-    # print(month_arr)
-    # df = pd.DataFrame({'Career': export_career, 'job': arr})
+
     arr = np.where(np.array(arr) == 0, 0, arr)
     plt.figure(figsize=(20, 10))
     plt.pie(arr, labels=None, autopct='')
@@ -56,5 +66,40 @@ def exported_career(career):
     plt.savefig(file_path)
     plt.close()
 
+def exported_follow_skills(career):
+    arr = []
+    label = []
+    top_skill = []
+    value = []
+    for skill in skills:
+        export_list = []
+        query = {
+            'career': career,
+            'detail.skills': skill
+        }
+
+        for item in exporter.find_job(query):
+            export_list.append(item)
+        if export_list:
+            count = len(export_list)
+        else:
+            count = 0
+        if count != 0:
+            label.append(skill)
+            arr.append(count)   
+        for i in range(len(arr)-1):
+            if arr[i] <= arr[i+1]:
+                tam = arr[i]
+                arr[i] = arr[i+1]
+                arr[i+1] = tam
+                
+                tam = label[i]
+                label[i] = label[i+1]
+                label[i+1] = tam
+    for i in range(11):             
+        top_skill.append(label[i])
+        value.append(arr[i])
+    print(top_skill, value)
 for car in export_career:
-    exported_career(car)
+    exported_follow_area(car)
+    exported_follow_skills(car)
